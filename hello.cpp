13276ab9c2f22f497c2e454ce5af47ad0b0d90a4
李燕飞 <16495444+evilbear_lyf@user.noreply.gitee.com>
2026-02-25 11:16:34 +0800

    增加test2

--- hello.cpp
+++ hello.cpp
@@ -2,9 +2,7 @@
 using namespace std;
 int main(){
 cout<<"HelloWorld"<<endl;
-<<<<<<< HEAD
 cout<<"Test"<<endl;
-=======
->>>>>>> c4b8a4de47fc54581cf846957e153441001ac2c8
+cout<<"test2"<<endl;
 return 0;
 }
